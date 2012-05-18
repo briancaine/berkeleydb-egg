d036a79a9806a73554b17dbe375e84db69ef30f1
Brian Caine <brian.d.caine@gmail.com>
2012-05-18 17:18:45 -0400

    Hooray, db-cursor is working

--- berkeleydb-src.scm
+++ berkeleydb-src.scm
@@ -1,6 +1,6 @@
 (import chicken scheme)
 (import foreign)
-(use lolevel ports)
+(use lolevel ports srfi-4)
 
 (foreign-declare
   "#include <db.h>
@@ -15,6 +15,7 @@
 
 (define DB_BTREE (foreign-value "DB_BTREE" int))
 (define DB_HEAP (foreign-value "DB_HEAP" int))
+(define DB_HASH (foreign-value "DB_HASH" int))
 (define DB_QUEUE (foreign-value "DB_QUEUE" int))
 (define DB_RECNO (foreign-value "DB_RECNO" int))
 (define DB_UNKNOWN (foreign-value "DB_UNKNOWN" int))
@@ -26,16 +27,45 @@
 (define DB_AUTO_COMMIT (foreign-value "DB_AUTO_COMMIT" int))
 
 (define EINVAL (foreign-value "EINVAL" int))
+(define ENOENT (foreign-value "ENOENT" int))
 
 (define DB_BUFFER_SMALL (foreign-value "DB_BUFFER_SMALL" int))
 
+(define DB_NOTFOUND (foreign-value "DB_NOTFOUND" int))
+
+(define DB_NEXT (foreign-value "DB_NEXT" int))
+
+(define DB_CREATE (foreign-value "DB_CREATE" int))
+
 (define +db-pointer-size+
   (max (foreign-value "sizeof(DB**)" int)
-       (foreign-value "sizeof(DB*)" int)))
+       (foreign-value "sizeof(DB*)" int)
+       (foreign-value "sizeof(DBT*)" int)))
+(define +dbt-size+ (foreign-value "sizeof(DBT)" int))
+
+(define dbt->string
+  (let ((DBT*-data (foreign-lambda* c-pointer ((c-pointer x))
+                                    "C_return(((DBT*)x)->data);"))
+        (DBT*-size (foreign-lambda* unsigned-int ((c-pointer x))
+                                    "C_return(((DBT*)x)->size);")))
+    (lambda (x)
+      (let* ((size (DBT*-size x))
+             (res (make-string size)))
+        (move-memory! (DBT*-data x) res size)
+        res))))
 
 (define DB*-pointer-ref
   (foreign-lambda* c-pointer ((c-pointer x))
     "C_return(x);"))
+
+(define DB**-pointer-ref
+  (foreign-lambda* c-pointer ((c-pointer x))
+    "C_return(*(DB**)x);"))
+
+(define DBC*-pointer-ref
+  (foreign-lambda* c-pointer ((c-pointer x))
+    "DBC** ptr = x;
+     C_return(*ptr);"))
 
 (define (data->string x len)
   (let ((res (make-string len)))
@@ -56,7 +86,7 @@
     (lambda (#!key dbenv (flags 0))
       (let* ((res-ptr (allocate (* +db-pointer-size+ 2)))
              (result (int-db-create res-ptr dbenv flags))
-             (final-ptr (let ((x (DB*-pointer-ref res-ptr)))
+             (final-ptr (let ((x (DB**-pointer-ref res-ptr)))
                           (free res-ptr)
                           x)))
         (cond
@@ -83,14 +113,24 @@
                    (string-join (map (compose ->string cadr) vars) ", ")))))))
 
 (define db-open!
-  (let ((int-db-open (db-func int open c-pointer c-pointer c-string c-string
-                              unsigned-int unsigned-int int)))
+  (let (
+        (int-db-open (foreign-lambda*
+                      int ((c-pointer db) (c-pointer txnid) (c-string filename)
+                           (c-string database) (unsigned-int type)
+                           (unsigned-int flags) (int mode))
+                      "C_return(((DB*)db)->open(
+                         db, txnid, filename, database, type, flags, mode));"))
+;        (int-db-open (db-func int open c-pointer c-pointer c-string c-string
+;                              unsigned-int unsigned-int int))
+        )
     (lambda (db filename #!key txnid database (type DB_UNKNOWN) (mode 0)
                                (flags 0))
       (let ((res (int-db-open (berkeley-db-ptr db)
                     txnid filename database type flags mode)))
         (cond
           ((zero? res) #t)
+          ((equal? res ENOENT)
+           (error "File or directory doesn't exist" filename))
           (else (error "Some sort of error")))))))
 
 (define db-put!
@@ -101,6 +141,8 @@
               (c-string data) (int datalen)
               (int flags))
              "DBT a, b;
+              memset(&a, 0, sizeof(DBT));
+              memset(&b, 0, sizeof(DBT));
               a.data = key;
               a.size = keylen;
               b.data = data;
@@ -120,21 +162,29 @@
            (foreign-lambda* int
              ((c-pointer db) (c-pointer txnid)
               (c-string key) (int keylen)
-              (c-string data) (int datalen)
+              (c-pointer data) (int datalen) (u32vector final_size)
               (int flags))
              "DBT a, b;
+              memset(&a, 0, sizeof(DBT));
+              memset(&b, 0, sizeof(DBT));
               a.data = key;
               a.size = keylen;
               b.data = data;
-              b.size = datalen;
-              C_return(((DB*)db)->get(db, txnid, &a, &b, flags));")))
+              b.ulen = datalen;
+              b.flags = DB_DBT_USERMEM;
+              int res = ((DB*)db)->get(db, txnid, &a, &b, flags);
+              *(uint*)final_size = b.size;
+              C_return(res);")))
     (lambda (db key #!key txnid (flags 0))
       (define (try count)
         (let* ((data (allocate count))
+               (res-size (make-u32vector 1))
                (res (int-db-get (berkeley-db-ptr db) txnid
                                 key (string-length key)
-                                data count flags))
-               (res-str (and (zero? res) (data->string data count))))
+                                data count res-size
+                                flags))
+               (res-str (and (zero? res)
+                             (data->string data (u32vector-ref res-size 0)))))
           (free data)
           (cond
             ((zero? res) res-str)
@@ -142,4 +192,69 @@
             (else (error "Unknown error")))))
       (try +default-allocation+))))
 
+(define db-delete!
+  (let ((int-db-del
+           (foreign-lambda* int
+             ((c-pointer db) (c-pointer txnid)
+              (c-string key) (int keylen)
+              (int flags))
+             "DBT a;
+              memset(&a, 0, sizeof(DBT));
+              a.data = key;
+              a.size = keylen;
+              C_return(((DB*)db)->del(db, txnid, &a, flags));")))
+    (lambda (db key #!key txnid (flags 0))
+      (let ((res (int-db-del (berkeley-db-ptr db) txnid
+                             key (string-length key)
+                             flags)))
+        (cond
+          ((zero? res) #t)
+          (else (error "Some sort of error")))))))
 
+;; cursors
+(define-record berkeley-db-cursor ptr db)
+(define-record-printer (berkeley-db-cursor cursor port)
+  (with-output-to-port port
+     (lambda ()
+        (printf "#<berkeley-db-cursor ~a on ~s>"
+                (berkeley-db-cursor-ptr cursor)
+                (berkeley-db-cursor-db cursor)))))
+
+(define db-cursor
+  (let ((int-db-cursor
+         (foreign-lambda*
+          int ((c-pointer db) (c-pointer txnid)
+               (c-pointer cursorp) (unsigned-int flags))
+          "C_return(((DB*)db)->cursor(db, txnid, cursorp, flags));")))
+    (lambda (db #!key txnid (flags 0))
+      (let* ((ptr (allocate +db-pointer-size+))
+             (res (int-db-cursor (berkeley-db-ptr db) txnid ptr flags))
+             (cursor (DBC*-pointer-ref ptr)))
+        (free ptr)
+        (cond
+         ((zero? res) (make-berkeley-db-cursor cursor db))
+         (else (error "Some sort of error")))))))
+
+(define db-cursor-get/next
+  (let ((int-db-cursor-get/next
+         (foreign-lambda*
+          int ((c-pointer cursor)
+               (c-pointer key) (c-pointer data)
+               (unsigned-int flags))
+          "memset(key, 0, sizeof(DBT));
+           memset(data, 0, sizeof(DBT));
+           C_return(((DBC*)cursor)->get(cursor, key, data, flags));")))
+    (lambda (cursor #!key (flags DB_NEXT))
+      (let* ((key (allocate +dbt-size+))
+             (data (allocate +dbt-size+))
+             (res (int-db-cursor-get/next
+                   (berkeley-db-cursor-ptr cursor)
+                   key data flags))
+             (key-res (and (zero? res) (dbt->string key)))
+             (data-res (and (zero? res) (dbt->string data))))
+        (free key)
+        (free data)
+        (cond
+         ((zero? res) (cons key-res data-res))
+         ((equal? res DB_NOTFOUND) '())
+         (else (error "Some sort of error")))))))
--- berkeleydb.scm
+++ berkeleydb.scm
@@ -1,5 +1,12 @@
 (module berkeleydb
   (
    db-create berkeley-db-ptr berkeley-db-path
+   db-open! db-put! db-get db-delete!
+
+   db-cursor db-cursor-get/next
+
+   DB_CREATE
+
+   DB_BTREE DB_HEAP DB_HASH DB_QUEUE DB_RECNO DB_UNKNOWN
   )
   "berkeleydb-src.scm")
